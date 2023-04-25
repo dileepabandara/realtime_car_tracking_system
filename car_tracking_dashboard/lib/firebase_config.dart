fc4d475f553666706ae9f57dd9e97dd729e1b9f4
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2023-04-25 18:55:17 +0530

    Add car_tracking_dashboard

---
+++ car_tracking_dashboard/lib/firebase_config.dart
@@ -0,0 +1,11 @@
+class FirebaseConfig {
+  static const _apiKey = "AIzaSyDsNP8h9A3fl0cE_9uBNCmgMLzaoJt_1Z0";
+  static const _projectId = "car-tracking-dashboard";
+  static const _messagingSenderId = "870816318768";
+  static const _appId = "1:870816318768:web:58b448da886a7b6919b4f5";
+
+  String get apiKey => _apiKey;
+  String get projectId => _projectId;
+  String get messagingSenderId => _messagingSenderId;
+  String get appId => _appId;
+}
